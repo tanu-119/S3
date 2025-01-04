ec05ee0cddeb0e933a7946eb6de01423dfae1211
Devi Tanuja <166426256+tanu-119@users.noreply.github.com>
2025-01-04 10:48:36 +0530

    Update outputs.tf

--- S31/outputs.tf
+++ S31/outputs.tf
@@ -1,11 +1,11 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.id
 }
 
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
+output "bucket_arn" {
+  value = aws_s3_bucket.my_bucket.arn
 }
 
+output "bucket_region" {
+  value = aws_s3_bucket.my_bucket.region
+}
