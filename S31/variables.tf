1f2f66ce703d4f43dfe749539225a01e56c542a3
Devi Tanuja <166426256+tanu-119@users.noreply.github.com>
2025-01-04 10:39:10 +0530

    Update variables.tf

--- S31/variables.tf
+++ S31/variables.tf
@@ -1,18 +1,19 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-
-variable "region" {
-  description = "AWS region"
+# Define Variables
+variable "aws_region" {
+  description = "AWS Region for S3 bucket"
+  type        = string
   default     = "us-west-1"
 }
 
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
 }
 
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
+variable "acl" {
+  description = "Canned ACL for the bucket"
+  type        = string
+  default     = "private"
 }
-
